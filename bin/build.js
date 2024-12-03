08422eff84fee3b8889aebf0bc4f5a0d80ef3cd7
Adam Weston <awcodes1@gmail.com>
2024-12-03 14:57:45 -0500

    add output to esbuild script

--- bin/build.js
+++ bin/build.js
@@ -1,4 +1,5 @@
 import esbuild from 'esbuild'
+import fs from 'fs'
 
 const isDev = process.argv.includes('--dev')
 
@@ -11,6 +12,23 @@ async function compile(options) {
         await context.rebuild()
         await context.dispose()
     }
+}
+
+function convertBytes(bytes, options = {}) {
+    const { useBinaryUnits = false, decimals = 2 } = options;
+
+    if (decimals < 0) {
+        throw new Error(`Invalid decimals ${decimals}`);
+    }
+
+    const base = useBinaryUnits ? 1024 : 1000;
+    const units = useBinaryUnits
+        ? ["Bytes", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"]
+        : ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+
+    const i = Math.floor(Math.log(bytes) / Math.log(base));
+
+    return `${(bytes / Math.pow(base, i)).toFixed(decimals)} ${units[i]}`;
 }
 
 const defaultOptions = {
@@ -47,4 +65,15 @@ compile({
     ...defaultOptions,
     entryPoints: ['./resources/js/index.js'],
     outfile: './resources/dist/typist.js',
+}).then(() => {
+    if (!isDev) {
+        fs.stat('./resources/dist/typist.js', (err, stats) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+
+            console.log(`Total file size: ${convertBytes(stats.size)}`);
+        })
+    }
 })
